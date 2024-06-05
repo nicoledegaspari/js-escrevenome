8d2e8131a16fae7a760239f192c7d8fa4dc9fe88
nicoledegaspari <0000109092091xsp@al.educacao.sp.gov.br>
2024-06-05 09:23:04 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,15 @@
-let palavra;
-
 function setup() {
-  createCanvas(400, 400);
-
-  palavra = palavraAleatoria();
-  
-}
-
-function palavraAleatoria() {
-  
-  let palavras = ["Casa", "Apartamento", "Condominio"];
-  
-  return random(palavras);
-}
-
-function inicializaCores() {
-  background("brown");
-  fill("green");
-  textSize(64);
-  textAlign(CENTER, CENTER);
-}
-
-function palavraParcial(minimo, maximo) {
-  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
-  let parcial = palavra.substring(0, quantidade);
-  return parcial;
-}
-
-function draw() {
-  
-  inicializaCores();
-
-  let texto = palavraParcial(0, width);
-    
-  text(texto, 200, 200);
-  
-}
-
-function modoNoturno(horario) {
-  if (horario > 18) {
-    console.log("Você precisa ligar o modo escuro!");
-  } else {
-    console.log("Modo noturno não é necessário neste momento.");
+    createCanvas(600, 600);
+    background("white");
   }
-}
-
-modoNoturno(15);
-modoNoturno(20);
+  
+  function draw() {
+    
+  
+    stroke("yellow");
+    fill("pink");
+  
+    // console.log(mouseIsPressed);
+  
+    if (mouseIsPressed) rect(mouseX, mouseY, 20, 35);
+  }
